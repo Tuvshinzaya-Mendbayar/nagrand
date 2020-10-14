2341580e2f9189e117fd50fda81b7fa3f3b6b0cf
Tuvshinzaya Mendbayar <tuvshinzaya.mend@gmail.com>
2020-10-14 19:05:38 -0400

    Created new action provider and learning opts

--- webui/src/config.js
+++ webui/src/config.js
@@ -1,16 +1,19 @@
-import { createChatBotMessage } from 'react-chatbot-kit';
+import React from "react";
+import { createChatBotMessage } from "react-chatbot-kit";
 
-const config = { 
-  botName: "LearningBot",
-  initialMessages: [createChatBotMessage("Hi, I'm here to help. What do you want to learn?")],
-  customStyles: {
-    botMessageBox: {
-      backgroundColor: "#376B7E",
-    },
-    chatButton: {
-      backgroundColor: "#376B7E",
-    },
-  },
+import LearningOptions from "./components/LearningOptions/LearningOptions";
+
+const config = {
+initialMessages: [
+    createChatBotMessage("Hi, I'm here to help. What do you want to     learn?", {
+      widget: "learningOptions",
+    }),
+  ],
+ ...,
+ widgets: [
+     {
+      widgetName: "learningOptions",
+      widgetFunc: (props) => <LearningOptions {...props} />,
+     },
+ ],
 }
-
-export default config
