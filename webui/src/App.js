7fc95bce3cae4f7721a87480e1dbd99015d347ee
Tuvshinzaya Mendbayar <tuvshinzaya.mend@gmail.com>
2020-10-15 16:55:59 -0400

    learning configuration added

--- webui/src/App.js
+++ webui/src/App.js
@@ -6,6 +6,22 @@ import ActionProvider from './ActionProvider';
 import MessageParser from './MessageParser';
 import config from './config';
 
+const config = { 
+  botName: "LearningBot",
+  initialMessages: [createChatBotMessage("Hi, I'm here to help. What do you want to learn?")],
+  customStyles: {
+    botMessageBox: {
+      backgroundColor: "#376B7E",
+    },
+    chatButton: {
+      backgroundColor: "#376B7E",
+    },
+  },
+}
+
+export default config
+
+
 function App() {
   return (
     <div className="App">
